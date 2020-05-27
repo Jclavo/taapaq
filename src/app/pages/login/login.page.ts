5898d08fa5c7351480e04b45734cef03c8c46df7
Jclavo <jclavotafur@gmail.com>
2020-05-27 18:37:55 -0300

    create login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 //Models
 import { User } from 'src/app/models/user.model';
+import { Response } from "src/app/models/response.model";
+
+//Services
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: 'app-login',
@@ -12,9 +16,22 @@ export class LoginPage implements OnInit {
 
   public user = new User();
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.user);
+    this.userService.login(this.user).subscribe((data: Response) => {
+
+      console.log(data.message);
+      if (data.status) {
+        // console.log('logged');
+      }
+    },
+      error => { console.log('Received an error') }
+    );
   }
 
 }
