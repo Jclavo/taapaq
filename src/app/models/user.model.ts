e6a2aa0f1366d34f8c37a45c9f99bb0072d35a94
Jclavo <jclavotafur@gmail.com>
2020-05-27 21:58:37 -0300

    create function fromFata in userModel

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -4,4 +4,13 @@ export class User{
     name: string = '';
     email: string = '';
     password: string = '';
+
+    static fromData(_id,_name,_email){
+
+        let user = new User();
+        user.id = _id;
+        user.name = _name;
+        user.email = _email;
+        return user;
+    }
 }
