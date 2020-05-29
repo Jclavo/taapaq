99865793f4342369e903b7187cc27b36c9a53bd7
jose clavo tafur <jclavotafur@gmail.com>
2020-05-29 14:05:19 -0300

    set authentification when user sign in

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,7 +7,7 @@ import { Response } from "src/app/models/response.model";
 
 //Services
 import { UserService } from "src/app/services/user.service";
-
+import { AuthService } from "src/app/services/auth.service";
 //Env
 import { environment } from "src/environments/environment";
 @Component({
@@ -21,19 +21,27 @@ export class LoginPage implements OnInit {
   public appName = environment.appName;
 
   constructor(
-    private userService: UserService,
     private router: Router,
+    private userService: UserService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit() {
   }
 
+  ionViewDidEnter()
+  {
+    this.authService.setLoggedIn(false);
+  }
+
   login() {
     console.log(this.user);
-    this.userService.getAll().subscribe((data: Response) => {
-      console.log(data.message);
-      if (data.status) {
+    this.userService.getAll().subscribe((response: Response) => {
+      console.log(response.message);
+      if (response.status) {
         // console.log('logged');
+        this.authService.setLoggedIn(true);
+        this.authService.setUser(response.result);
         this.router.navigate(['/user-list']);
       }
     },
