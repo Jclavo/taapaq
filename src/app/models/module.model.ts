cb460cf197d0554f77ad108736ee667b5bd33a4e
Jclavo <jclavotafur@gmail.com>
2020-05-31 13:31:33 -0300

    add Resource to Module

--- src/app/models/module.model.ts
+++ src/app/models/module.model.ts
@@ -1,6 +1,9 @@
+import { Resource } from "src/app/models/resource.model";
+
 export class Module{
     id: number = 0;
     name: string = '';
     url: string = '';
     project_id: number = 0;
+    resources: Array<Resource> = []; 
 }
