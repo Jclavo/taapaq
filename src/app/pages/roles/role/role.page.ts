8f5e57f443fa6f948e6a96dd77a39d0f3051ac1f
jose clavo tafur <jclavotafur@gmail.com>
2020-06-01 17:53:31 -0300

    add list of project to page to add  a role

--- src/app/pages/roles/role/role.page.ts
+++ src/app/pages/roles/role/role.page.ts
@@ -4,9 +4,11 @@ import { Router } from '@angular/router';
 //Models
 import { Role } from "src/app/models/role.model";
 import { Response } from "src/app/models/response.model";
+import { Project } from "src/app/models/project.model";
 
 //Services
 import { RoleService } from "src/app/services/role.service";
+import { ProjectService } from "src/app/services/project.service";
 
 //Utils
 import { AuthUtils } from "src/app/utils/auth-utils";
@@ -20,12 +22,14 @@ import { MessageUtils } from "src/app/utils/message-utils";
 export class RolePage implements OnInit {
 
   public role = new Role();
+  public projects: Array<Project> = [];
 
   constructor(
     private roleService: RoleService,
     private authUtils: AuthUtils,
     private messageUtils: MessageUtils,
-    private router: Router
+    private router: Router,
+    private projectService: ProjectService,
   ) { }
  
   ngOnInit() {
@@ -33,6 +37,27 @@ export class RolePage implements OnInit {
 
   ionViewDidEnter(){
     !this.authUtils.isAuthenticated() ? this.authUtils.closeSession() : null; //It should be at any page to control session
+    
+    this.getAllProjects();
+  }
+
+  async getAllProjects() {
+    const loading = await this.messageUtils.createLoader();
+    loading.present();// start loading
+
+    this.projectService.getAll().subscribe((response: Response) => {
+      if (response.status) {
+        this.projects = response.result;}
+      else {
+        this.messageUtils.showToastError(response.message);
+      }
+      loading.dismiss();// close loading
+    },
+      error => {
+        this.messageUtils.showToastError(error.message);
+        loading.dismiss();// close loading
+      }
+    );
   }
 
   save(form){
