73d83079852b9948535ae7295c05fa860e65c216
Jclavo <jclavotafur@gmail.com>
2020-06-02 17:56:09 -0300

    create utils to findValue in array

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class Utils {
+
+    static findValueInCollection(collection: any[], searchValue: any) {
+
+        searchValue = searchValue?.toLowerCase();
+
+        return collection.filter(value => {
+            let flag: boolean = true
+            for (const key in value) {
+                if(!value[key]) value[key] = '';
+                value[key] = value[key].toString();
+
+                flag = value[key]?.toLowerCase().indexOf(searchValue) > -1 ? true : false;
+                if (flag) {
+                    return value;
+                }
+            }
+        });
+    }
+
+
+    //   static searchInCollection(collection: any[], arg: object | null) {
+
+    //     function callback(currentValue) {
+    //       let flag: boolean = true
+
+    //       for (const key in arg) {
+    //         if (flag) {
+    //           if (currentValue.hasOwnProperty(key)) {
+    //             if (currentValue[key] === null) {
+    //               currentValue[key] = '';
+    //             }
+
+    //             if (Number(currentValue[key])) {
+    //               currentValue[key] = currentValue[key].toString();
+    //             }
+    //             if (Number(arg[key])) {
+    //               arg[key] = arg[key].toString();
+    //             }
+    //             flag = (currentValue[key].toLowerCase().indexOf(arg[key].toLowerCase()) > -1) ? true : false;
+
+    //           }
+    //         }
+    //       }
+
+    //       if (flag) {
+    //         return currentValue
+    //       }
+    //     }
+    //     return collection.filter(callback, arg)
+    //   }
+}
