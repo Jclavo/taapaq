362ceebcc3395cef3770e208af735f5484341465
Jclavo <jclavotafur@gmail.com>
2020-06-01 21:09:54 -0300

    add users to CompanyModel

--- src/app/models/company.model.ts
+++ src/app/models/company.model.ts
@@ -1,8 +1,11 @@
 import { Project } from "src/app/models/project.model";
+import { User } from "src/app/models/user.model";
 
 export class Company{
     id: number = 0;
     name: string = '';
 
     projects: Array<Project> = [];
+    users: Array<User> = [];
+
 }
