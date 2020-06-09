b3a582aee010d0971cb9a67e72feb55775a997ae
Jclavo <jclavotafur@gmail.com>
2020-06-09 11:17:49 -0300

    add userdetails to User Model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,22 +1,27 @@
 import { Role } from "./role.model";
+import { UserDetail } from "./user-detail";
 
 export class User{
     id: number = 0;
     login: string = '';
-    password: string = '';
-    name: string = '';
-    email: string = '';
+    // password: string = '';
+    // name: string = '';
+    // email: string = '';
     isSuper: boolean = true;
+
     company_id: number = 0;
     project_id: number = 0;
+    user_detail_id: number = 0;
+
     roles: Array<Role> = [];
+    info = new UserDetail();
 
-    static fromData(_id,_name,_email){
+    // static fromData(_id,_name,_email){
 
-        let user = new User();
-        user.id = _id;
-        user.name = _name;
-        user.email = _email;
-        return user;
-    }
+    //     let user = new User();
+    //     user.id = _id;
+    //     user.name = _name;
+    //     user.email = _email;
+    //     return user;
+    // }
 }
