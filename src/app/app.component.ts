4e03643218657aee5455945560e7cb5065abc7d1
Jclavo <jclavotafur@gmail.com>
2020-06-11 19:01:13 -0300

    delete static lateral menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,71 +14,46 @@ import { AuthUtils } from "src/app/utils/auth-utils";
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
-  public appPages = [
-    {
-      title: 'Project',
-      url: '/project-list',
-      icon: 'rocket'
-    },
-    {
-      title: 'Companies',
-      url: '/company-list',
-      icon: 'business'
-    },
-    // {
-    //   title: 'Project - Company',
-    //   url: '/project-company',
-    //   icon: 'link'
-    // },
-    {
-      title: 'Modules',
-      url: '/module-list',
-      icon: 'globe'
-    },
-    {
-      title: 'Roles',
-      url: '/role-list',
-      icon: 'bookmarks'
-    },
-    // {
-    //   title: 'Permissions',
-    //   url: '/permission-list',
-    //   icon: 'key'
-    // },
-    {
-      title: 'Users',
-      url: '/user-list',
-      icon: 'people'
-    },
-    {
-      title: 'User Master',
-      url: '/user-detail-list',
-      icon: 'person'
-    },
 
-    // {
-    //   title: 'Archived',
-    //   url: '/folder/Archived',
-    //   icon: 'archive'
-    // },
-    // {
-    //   title: 'Trash',
-    //   url: '/folder/Trash',
-    //   icon: 'trash'
-    // },
-    // {
-    //   title: 'Spam',
-    //   url: '/folder/Spam',
-    //   icon: 'warning'
-    // }
-  ];
+  // public appPages = [
+  //   {
+  //     title: 'Project',
+  //     url: '/project-list',
+  //     icon: 'rocket'
+  //   },
+  //   {
+  //     title: 'Companies',
+  //     url: '/company-list',
+  //     icon: 'business'
+  //   },
+  //   {
+  //     title: 'Modules',
+  //     url: '/module-list',
+  //     icon: 'globe'
+  //   },
+  //   {
+  //     title: 'Roles',
+  //     url: '/role-list',
+  //     icon: 'bookmarks'
+  //   },
+  //   {
+  //     title: 'Users',
+  //     url: '/user-list',
+  //     icon: 'people'
+  //   },
+  //   {
+  //     title: 'User Master',
+  //     url: '/user-detail-list',
+  //     icon: 'person'
+  //   },
+  // ];
   public labels = ['Close session'];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private authUtils: AuthUtils
+    private authUtils: AuthUtils,
   ) {
     //Validate Session
     !this.authUtils.isAuthenticated() ? this.authUtils.closeSession() : null;
@@ -93,11 +68,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
-    console.log(this.authUtils.user);
+    // const path = window.location.pathname.split('folder/')[1];
+    // if (path !== undefined) {
+    //   this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    // }
+    // console.log(this.authUtils.user);
   }
 
   closeSession(){
