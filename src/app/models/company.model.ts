8e28005db1fe36201ead730e8dcd8777378a3448
Jclavo <jclavotafur@gmail.com>
2020-06-27 23:40:43 -0300

    add country to company model

--- src/app/models/company.model.ts
+++ src/app/models/company.model.ts
@@ -1,5 +1,6 @@
 import { Project } from "src/app/models/project.model";
 import { User } from "src/app/models/user.model";
+import { Country } from "src/app/models/country.model";
 
 export class Company{
     id: number = 0;
@@ -8,5 +9,6 @@ export class Company{
 
     projects: Array<Project> = [];
     users: Array<User> = [];
+    country = new Country();
 
 }
