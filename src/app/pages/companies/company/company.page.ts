7a72757e1be412ec03fccb8af7487b2df39b5108
Jclavo <jclavotafur@gmail.com>
2020-06-27 23:03:45 -0300

    when company is created, add the country

--- src/app/pages/companies/company/company.page.ts
+++ src/app/pages/companies/company/company.page.ts
@@ -4,9 +4,11 @@ import { Router } from '@angular/router';
 //Models
 import { Response } from "src/app/models/response.model";
 import { Company } from "src/app/models/company.model";
+import { Country } from "src/app/models/country.model";
 
 //Services
 import { CompanyService } from "src/app/services/company.service";
+import { CountryService } from "src/app/services/country.service";
 
 //Utils
 import { AuthUtils } from "src/app/utils/auth-utils";
@@ -20,12 +22,14 @@ import { MessageUtils } from "src/app/utils/message-utils";
 export class CompanyPage implements OnInit {
 
   public company = new Company();
+  public countries: Array<Country> = [];
 
   constructor(
     private companyService: CompanyService,
     private authUtils: AuthUtils,
     private messageUtils: MessageUtils,
-    private router: Router
+    private router: Router,
+    private countryService: CountryService
   ) { }
  
   ngOnInit() {
@@ -33,6 +37,8 @@ export class CompanyPage implements OnInit {
 
   ionViewDidEnter(){
     !this.authUtils.isAuthenticated() ? this.authUtils.closeSession() : null; //It should be at any page to control session
+
+    this.getCountries();
   }
 
   save(){
@@ -42,6 +48,26 @@ export class CompanyPage implements OnInit {
     }else{
       this.create(this.company);
     }
+  }
+
+  async getCountries() {
+    const loading = await this.messageUtils.createLoader();
+    loading.present();// start loading
+
+    this.countryService.getAll().subscribe((response: Response) => {
+      if (response.status) {
+        this.countries = response.result;
+      }
+      else {
+        this.messageUtils.showToastError(response.message);
+      }
+      loading.dismiss();// close loading
+    },
+      error => {
+        this.messageUtils.showToastError(error.message);
+        loading.dismiss();// close loading
+      }
+    );
   }
 
   async create(company: Company){
