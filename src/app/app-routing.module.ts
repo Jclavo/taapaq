e2fb3cfa02728d3d407d5a67845f2cbe3ecfca7f
Jclavo <jclavotafur@gmail.com>
2020-09-16 20:11:40 -0300

    add page to show not found when route not exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ const routes: Routes = [
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   // {
   //   path: 'user',
@@ -25,84 +25,93 @@ const routes: Routes = [
   // },
   {
     path: 'role-list',
-    loadChildren: () => import('./pages/roles/role-list/role-list.module').then( m => m.RoleListPageModule)
+    loadChildren: () => import('./pages/roles/role-list/role-list.module').then(m => m.RoleListPageModule)
   },
   {
     path: 'role-list/:project_id',
-    loadChildren: () => import('./pages/roles/role-list/role-list.module').then( m => m.RoleListPageModule)
+    loadChildren: () => import('./pages/roles/role-list/role-list.module').then(m => m.RoleListPageModule)
   },
   {
     path: 'role-permission/:id',
-    loadChildren: () => import('./pages/roles/role-permission/role-permission.module').then( m => m.RolePermissionPageModule)
+    loadChildren: () => import('./pages/roles/role-permission/role-permission.module').then(m => m.RolePermissionPageModule)
   },
   {
     path: 'role/:project_id',
-    loadChildren: () => import('./pages/roles/role/role.module').then( m => m.RolePageModule)
+    loadChildren: () => import('./pages/roles/role/role.module').then(m => m.RolePageModule)
   },
   {
     path: 'project-list',
-    loadChildren: () => import('./pages/projects/project-list/project-list.module').then( m => m.ProjectListPageModule)
+    loadChildren: () => import('./pages/projects/project-list/project-list.module').then(m => m.ProjectListPageModule)
   },
   {
     path: 'project',
-    loadChildren: () => import('./pages/projects/project/project.module').then( m => m.ProjectPageModule)
+    loadChildren: () => import('./pages/projects/project/project.module').then(m => m.ProjectPageModule)
   },
   {
     path: 'module-list',
-    loadChildren: () => import('./pages/modules/module-list/module-list.module').then( m => m.ModuleListPageModule)
+    loadChildren: () => import('./pages/modules/module-list/module-list.module').then(m => m.ModuleListPageModule)
   },
   {
     path: 'module-list/:project_id',
-    loadChildren: () => import('./pages/modules/module-list/module-list.module').then( m => m.ModuleListPageModule)
+    loadChildren: () => import('./pages/modules/module-list/module-list.module').then(m => m.ModuleListPageModule)
   },
   {
     path: 'module/:project_id',
-    loadChildren: () => import('./pages/modules/module/module.module').then( m => m.ModulePageModule)
+    loadChildren: () => import('./pages/modules/module/module.module').then(m => m.ModulePageModule)
   },
   {
     path: 'resource/:project_id/:module_id',
-    loadChildren: () => import('./pages/resources/resource/resource.module').then( m => m.ResourcePageModule)
+    loadChildren: () => import('./pages/resources/resource/resource.module').then(m => m.ResourcePageModule)
   },
   {
     path: 'company-list',
-    loadChildren: () => import('./pages/companies/company-list/company-list.module').then( m => m.CompanyListPageModule)
+    loadChildren: () => import('./pages/companies/company-list/company-list.module').then(m => m.CompanyListPageModule)
   },
   {
     path: 'company',
-    loadChildren: () => import('./pages/companies/company/company.module').then( m => m.CompanyPageModule)
+    loadChildren: () => import('./pages/companies/company/company.module').then(m => m.CompanyPageModule)
   },
   {
     path: 'project-company',
-    loadChildren: () => import('./pages/projects/project-company/project-company.module').then( m => m.ProjectCompanyPageModule)
+    loadChildren: () => import('./pages/projects/project-company/project-company.module').then(m => m.ProjectCompanyPageModule)
   },
   {
     path: 'project-company/:project_id',
-    loadChildren: () => import('./pages/projects/project-company/project-company.module').then( m => m.ProjectCompanyPageModule)
+    loadChildren: () => import('./pages/projects/project-company/project-company.module').then(m => m.ProjectCompanyPageModule)
   },
   {
     path: 'user-list',
-    loadChildren: () => import('./pages/users/user-list/user-list.module').then( m => m.UserListPageModule)
+    loadChildren: () => import('./pages/users/user-list/user-list.module').then(m => m.UserListPageModule)
   },
   {
     path: 'user-list/:project_id/:company_id',
-    loadChildren: () => import('./pages/users/user-list/user-list.module').then( m => m.UserListPageModule)
+    loadChildren: () => import('./pages/users/user-list/user-list.module').then(m => m.UserListPageModule)
   },
   {
     path: 'user/:project_id/:company_id',
-    loadChildren: () => import('./pages/users/user/user.module').then( m => m.UserPageModule)
+    loadChildren: () => import('./pages/users/user/user.module').then(m => m.UserPageModule)
   },
   {
     path: 'user-detail',
-    loadChildren: () => import('./pages/users/user-detail/user-detail.module').then( m => m.UserDetailPageModule)
+    loadChildren: () => import('./pages/users/user-detail/user-detail.module').then(m => m.UserDetailPageModule)
   },
   {
     path: 'user-detail-list',
-    loadChildren: () => import('./pages/users/user-detail-list/user-detail-list.module').then( m => m.UserDetailListPageModule)
+    loadChildren: () => import('./pages/users/user-detail-list/user-detail-list.module').then(m => m.UserDetailListPageModule)
   },
   {
     path: 'module-label/:project_id',
-    loadChildren: () => import('./pages/modules/module-label/module-label.module').then( m => m.ModuleLabelPageModule)
-  }
+    loadChildren: () => import('./pages/modules/module-label/module-label.module').then(m => m.ModuleLabelPageModule)
+  },
+  {
+    path: 'not-found',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
@@ -111,4 +120,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
